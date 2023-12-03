b53bd6d152dc1a81da5e0d5b9d6bdcc95a1beee2
manQaTester <sinm672542gmail.com>
2023-12-03 15:53:16 +0530

    first comment - method one

--- src/qatester/man/ManEmulator.java
+++ src/qatester/man/ManEmulator.java
@@ -3,6 +3,11 @@ package qatester.man;
 public class ManEmulator {
 	public static void main(String[] args) {
 		System.out.println("hi man");
+		methodOne();
+	}
+	
+	public static  void methodOne() {
+		System.out.println("hi this is method one");
 	}
 
 }
