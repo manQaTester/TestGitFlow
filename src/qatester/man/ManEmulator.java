757ecc496cce145509963409a51011625077572e
manQaTester <sinm672542gmail.com>
2023-12-03 16:05:05 +0530

    second commit -method two

--- src/qatester/man/ManEmulator.java
+++ src/qatester/man/ManEmulator.java
@@ -2,12 +2,17 @@ package qatester.man;
 
 public class ManEmulator {
 	public static void main(String[] args) {
-		System.out.println("hi man");
+		System.out.println("hi man - this is main method");
 		methodOne();
+		methodTwo();
 	}
 	
 	public static  void methodOne() {
 		System.out.println("hi this is method one");
+	}
+	
+	public static void methodTwo() {
+		System.out.println("hi this is method two");
 	}
 
 }
